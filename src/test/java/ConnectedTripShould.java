57b220fed1e9b1171c7ccc41767141839b7beb64
jjwils <mail@johnwilson.me.uk>
2023-08-29 10:54:51 +0100

    and the rest

---
+++ src/test/java/ConnectedTripShould.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConnectedTripShould {
+    
+    
+    
+    @Test
+    public void one_equal_one () throws Exception{
+        assertEquals(1,1);
+    }
+}
