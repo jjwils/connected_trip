0442af0d56714994826759905799ec41c67341cf
salims1 <salim.shaikh@booking.com>
2023-09-01 15:53:45 +0530

    adedd driverA test

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+V
+&src/test/java/ConnectedTripShould.java,e/9/e942b9055e158a68f4c17732fa16c3b2a77283b3
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- src/test/java/ConnectedTripShould.java
+++ src/test/java/ConnectedTripShould.java
@@ -1,13 +1,19 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConnectedTripShould {
-    
-    
-    
+
+
+
     @Test
     public void one_equal_one () throws Exception{
         assertEquals(1,1);
+    }
+
+    @Test
+    public void driverA(){
+        assertTrue();
     }
 }
