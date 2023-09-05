8f007dcb89f44b86d10ad8a659e57fb7bf28f259
varshasarode <varsharani.sarode@booking.com>
2023-09-05 17:07:12 +0530

    added test for taxi

--- src/test/java/TaxiShould.java
+++ src/test/java/TaxiShould.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TaxiShould {
 
     @Test
-    void not_have_passenger(){
+    void not_have_passenger() {
         // Arrange
         Taxi taxi = new Taxi();
 
@@ -13,5 +14,11 @@ public class TaxiShould {
 
         // Assert
         assertFalse(taxi.passengerOnBoard());
+    }
+
+    @Test
+    void have_passenger() {
+        Taxi taxi = new Taxi();
+        assertTrue(taxi.passengerOnBoard());
     }
 }
